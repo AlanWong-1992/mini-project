bdb1000397dfdac940ac574257fbcd172acfc90f
Alan <alan_wong888@yahoo.com>
2021-10-24 14:24:34 +0100

    added .devcontainer and docker-compose for MySQL and adminer containers

--- src/menu-2.py
+++ src/menu-2.py
@@ -22,6 +22,24 @@ class Menu:
                 input_is_correct = False
                 
         return choice
-        
-menu = Menu()
-menu.main_menu_choice()
+
+    def sub_menu_choice(item):
+        input_is_correct = False
+        while input_is_correct == False:
+            try:
+                choice = int(input('\nPlease choose an option by selecting a number:\n'
+                                    '0) Return to previous menu\n'
+                                    f'1) Add a {item}\n'
+                                    f'2) View all {item}s\n'
+                                    f'3) Update a {item}\n'
+                                    f'4) Delete a {item}\n'
+                                    'Please select a number from the above options: '))
+                if (type(choice) == int and choice <=4 and choice >=0):
+                    input_is_correct = True
+                else:
+                    print('You need to pick one of the numbers above')
+            except ValueError:
+                print('You need to enter one of the numbers above')
+                input_is_correct = False
+
+        return choice
